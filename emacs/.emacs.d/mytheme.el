78d295ab657ad7a54cafc32986aeda57ba9a6316
lenxyang <lenxyang@gmail.com>
2013-12-31 11:26:29 +0800

    add cdb and something elase

--- emacs/.emacs.d/mytheme.el
+++ emacs/.emacs.d/mytheme.el
@@ -12,49 +12,52 @@
      ((foreground-color . "wheat")
       (background-color . "black")
       (background-mode . dark))
-     ; set font
-     ;(default ((t (nil))))
-     (default ((t (
-		   :stipple nil :background "black" :foreground "grep80"
-			    :inverse-video nil :box nil :strike-through nil
-			    :overline nil :underline nil :slant normal
-			    :weight bold :height 360 :width normal
-			    :family "Courier New"))))
-     (fixed ((t (:bold t))))
-     (bold ((t (:bold t))))
-     (italic ((t (:italic t))))
-     (bold-italic ((t (:bold t :italic t))))
-     (underline ((t (:underline t))))
-     (region ((t (:foreground "gray95" :background "blue"))))
-     (secondary-selection ((t (:background "grep 60"))))
-     (underline ((t (:underline t))))
-     (font-lock-builtin-face ((t (:bold t :foreground "cyan1" :bold t))))
-     (font-lock-comment-face ((t (:foreground "IndianRed" :italic t))))
-     (font-lock-constant-face ((t (:foreground "magenta3"))))
-     (font-lock-doc-face ((t (:foreground "cyan"))))
-     (font-lock-doc-string-face ((t (:foreground "light green"))))
-     (font-lock-function-name-face ((t (:bold t :foreground "light green"))))
-     (font-lock-keyword-face ((t (:foreground "cyan1" :bold t))))
-     (font-lock-preprocessor-face ((t (:foreground "blue1"))))
-     (font-lock-reference-face ((t (:foreground "purple1"))))
-     (font-lock-string-face ((t (:foreground "LimeGreen" :italic t))))
+      (font-lock-builtin-face ((t (:foreground "cyan1"))))
+      (font-lock-comment-face ((t (:foreground "IndianRed" :italic t))))
+      (font-lock-constant-face ((t (:foreground "magenta3"))))
+      (font-lock-type-face ((t (:foreground "green"))))
+      (font-lock-preprocessor-face ((t (:foreground "blue1"))))
+      (font-lock-doc-face ((t (:foreground "cyan"))))
+      (font-lock-doc-string-face ((t (:foreground "light green"))))
+      (font-lock-function-name-face ((t (:foreground "light green"))))
+      (font-lock-keyword-face ((t (:foreground "cyan1"))))
+      (font-lock-reference-face ((t (:foreground "purple1"))))
+      (font-lock-string-face ((t (:foreground "IndianRed"))))
+      (font-lock-variable-name-face ((t (:foreground "gray80"))))
+      (font-lock-warning-face ((t (:foreground "red"))))
+     
+      (default ((t (:stipple nil :background "black" :foreground "gray80"
+			     :inverse-video nil :box nil :strike-through nil
+			     :overline nil :underline nil :slant normal
+			     :weight bold :height 360 :width normal
+			     :family "Courier New"))))
+      (fixed ((t (:bold t))))
+      (bold ((t (:bold t))))
+      (italic ((t (:italic t))))
+      (bold-italic ((t (:bold t :italic t))))
+      (underline ((t (:underline t))))
+      (region ((t (:foreground "gray95" :background "blue"))))
+      (secondary-selection ((t (:background "grep 60"))))
+      (underline ((t (:underline t))))
 
-     (font-lock-type-face ((t (:bold t :foreground "gray80"))))
-     (font-lock-variable-name-face ((t (:bold t :foreground "gray80"))))
-     (font-lock-warning-face ((t (:bold t :foreground "red" :weight bold))))
+      (highlight ((t (:background "darkolivegreen"))))
+      (isearch ((t (:background "palevioletred2" :foreground "brown4"))))
+      (isearch-lazy-highlight-face ((t (:background "paleturquoise4"))))
 
-     (highlight ((t (:background "darkolivegreen"))))
-     (isearch ((t (:background "palevioletred2" :foreground "brown4"))))
-     (isearch-lazy-highlight-face ((t (:background "paleturquoise4"))))
-
-     ; mode line
-     (mode-line ((t (:background "gray40" :foreground "gray90"))))
-     (mode-line-inactive ((t (:backgro1und "gray16" :foreground "gray60"))))
-     (modeline-buffer-id ((t (:background "gray16" :foreground "gray80"))))
-     (modeline-mousable ((t (:background "gray16" :foreground "gray80"))))
-     (modeline-mousable-minor-mode ((t (:background "gray16" :foreground "gray80" ))))
-     )))
+      (mode-line ((t (:background "gray40" :foreground "gray90"
+				  :box (:line-width -1 :style released-button)
+				  :overline "gray60" :underline "gray60"))))
+      (mode-line-inactive ((t (:italic t :underline "gray60"
+				       :overline "gray60"
+				       :background "gray16"
+				       :foreground "cadetblue"
+				       :box (:line-width -1 :color "grey75")
+				       :slant oblique :weight light))))
+      (modeline ((t (:background "gray40" :foreground "white" :box (:line-width -1 :style released-button) :overline "gray60" :underline "gray60"))))
+      (modeline-buffer-id ((t (:background "gray16" :foreground "cyan1" :box (:line-width -1 :style released-button) :overline "gray60" :underline "gray60"))))
+      (modeline-mousable ((t (:background "gray16" :foreground "cyan1" :box (:line-width -1 :style released-button) :overline "gray60" :underline "gray60"))))
+      (modeline-mousable-minor-mode ((t (:background "gray16" :foreground "cyan1" :box (:line-width -1 :style released-button) :overline "gray60" :underline "gray60"))))
+      )))
 
 (color-theme-lenxyang)
-
 
--- emacs/.emacs.d/win32_init.el
+++ emacs/.emacs.d/win32_init.el
@@ -1,29 +1,61 @@
-
-'(compile-command "ninja")
-
 (scroll-bar-mode -1)
 (menu-bar-mode 0)
-(tool-bar-mode 0)   
+(tool-bar-mode 0)
+'(compile-command "ninja")
 (load-file "~/.emacs.d/mytheme.el")
+(set-face-attribute `default nil :font "Courier New 14")
+(prefer-coding-system 'utf-8)
+(set-fontset-font "fontset-default"
+		  'gb18030 '("Microsoft YaHei" . "unicode-bmp"))
 
-; 设置默认字体
-(set-face-attribute `default nil :font "Courier New 12")
 
-; 设置启动后最大化
-(defun maximize-frame () 
+; mouse select to copy
+(setq mouse-yank-at-point t)
+
+; 鏈�澶у寲绐楀彛
+(defun maximize-frame (&optional frame) 
    "Maximizes the active frame in Windows" 
    (interactive) 
    ;; Send a `WM_SYSCOMMAND' message to the active frame with the 
    ;; `SC_MAXIMIZE' parameter. 
    (when (eq system-type 'windows-nt) 
-     (w32-send-sys-command 61488))) 
-(add-hook 'window-setup-hook 'maximize-frame t) 
-
-; mouse select to copy
-
-; mouse right-click to paste
+     (w32-send-sys-command 61488)))
 
 
+(add-hook 'window-setup-hook 'maximize-frame t)
+(add-hook 'after-make-frame-functions 'maximize-frame)
 
+; use cdb
+(load-file "~/.emacs.d/site-lisp/cdb-gud.el")
+(load-file "~/.emacs.d/site-lisp/sr_speedbar.el")
+(setq speedbar-use-images nil)
+(setq sr-speedbar-max-width 50)
+(setq sr-speedbar-width-x 50)
+(setq sr-speedbar-width-console 40)
+(setq sr-speedbar-right-side nil)
+(setq sr-speedbar-skip-other-window-p t)
 
+; F12 open or close speedbar
+(defun my-speedbar-switch()
+  ""
+  (interactive)
+  (if (not (sr-speedbar-exist-p))
+      (progn
+	(sr-speedbar-open)
+	(sr-speedbar-select-window)
+	)
+    (progn
+      (sr-speedbar-select-window)
+      )
+    )
+)
 
+(sr-speedbar-open)
+(with-current-buffer sr-speedbar-buffer-name
+  (setq window-size-fixed 'width))
+
+; ATL-F4 to close
+(global-set-key [M-f4] 'save-buffers-kill-emacs)
+(global-set-key [f12] 'my-speedbar-switch)
+(global-set-key [f5] 'maximize-frame)
+(global-set-key [C-tab] 'other-frame)
